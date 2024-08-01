702f432899842cb41dd86f8bb030771ba523f639
Subhashini <145525075+SSSubhashini@users.noreply.github.com>
2024-08-01 21:21:51 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historical places in India</title>
+    <style>
+        header{
+            color:rgb(245, 246, 248);
+            font-family:Georgia, 'Times New Roman', Times, serif;
+            font-size: x-large;
+            position: fixed;
+            background-position:1 75px;
+            box-shadow: 10px 10px 10px black;
+            left:0px;
+            top:2px;
+            width:100%;
+            background-size: cover;
+            background-image: url("https://mannatresorts.com/wp-content/uploads/2018/09/footer-background-1.png");
+        }
+        .img{
+            position: -ms-page;
+            top:500px;
+            left:5px;
+            height:350px;
+            width: 500px;
+        }
+        #pic{
+    
+            color: rgb(11, 11, 11);
+            background-image:url("https://img.freepik.com/premium-photo/yellow-green-mixed-gradient-color-vertical-background_7954-30483.jpg?w=2000");
+            background-size: cover;
+            
+        }
+        .img1{
+            position: -ms-page;
+            margin-top:50px;
+            left:5px;
+            height:305px;
+            width:100%;
+         }
+         #web{
+            width:70%;
+            text-align: center;
+            background-color:white;
+            padding-left: 2%;
+            padding-right: 2%;
+         }
+         footer{
+            color:whitesmoke;
+            background-image: url("https://img.lovepik.com/background/20211021/large/lovepik-blue-geometric-business-background-image_401526801.jpg");
+            font-size:large;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+            position: inherit;
+            bottom: 12px;
+            width:100%;
+            height:10%;
+            text-align: center;
+            margin-bottom: 3%;
+            padding-bottom: 3%;
+            padding-top: 3%;
+            box-shadow: 10px 10px 10px black;
+            
+         }
+         #han{
+            color: rgb(11, 11, 11);
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+            font-size: larger;
+        }
+        </style>
+</head>
+
+    <div align="center">
+    <header>
+       <h2>HISTORICAL PLACES IN INDIA </h2> 
+    </header>
+    <br><br><br><br>
+    <body id="pic">
+    <div id="web"> 
+    <img class="img1" src="https://media.istockphoto.com/id/165977889/vector/india.jpg?s=170667a&w=0&k=20&c=VwqxAoMIF19kLt95uOgQwsXIb02Gv4L7Wl-vp7CE3wI=">  
+    <p align="justify" id="han">
+        
+        5000 years of civilization and more has given India some fantastic historical places, monuments, legends and experiences. 
+        Paying a tribute to the glorious history and heritage, these monuments are absolutely fascinating. Right from ancient to medieval times, 
+        several kings and emperors, dynasties and kingdoms have built monuments for various reasons. However, 
+        there is no denying the fact that historical places in India draw visitors in large numbers. From the Taj Mahal to 
+        the ruins of Hampi, every fort, palace or temple is an example of outstanding aesthetics and elegance.
+    </p>
+    <h1 align="left"><b>A list of historical places to visit in India</b></h1>
+    <h2 align="left">1.<u>Taj Mahal,UttarPradhesh</u></h2>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-12 box">
+                <img class="img" src="https://i.natgeofe.com/n/8eba070d-14e5-4d07-8bab-9db774029063/93080_4x3.jpg" alt="Taj Mahal">
+                <p align="left" id="han">
+                    <b><i>Taj Mahal</i></b>,a UNESCO World Heritage Site, is one of the most famous historical places in Agra city of Uttar Pardesh and should be visited at least once in a lifetime. It represents the pinnacle of grand Mughal
+                    architecture and is built entirely of white marble. Shah Jahan built this magnificent monument for one of his favorite wife, Mumtaz Mahal. Visitors 
+                    to this grand monument are left captivated by its grandeur and perfect aesthetical proportions. It took around 22 years to complete the construction of this splendid monument.<br>
+
+                    <b>Interesting Fact About Taj Mahal Agra:</b> There is a shopping complex and hotel being built in Dubai which is inspired by the Taj Mahal. It’s estimated to be four times larger in size than the original Taj Mahal.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <h2 align="left">2.<u>Hampi,Karnataka</u></h2>
+            <div class="col-12 box">
+                <img class="img" src="https://hindi.nativeplanet.com/img/2017/11/1-30-1512025019.jpg" alt="Hampi">
+                <p align="left" id="han">
+                    The ruins of <b><i>Hampi</i></b>, a UNESCO World Heritage Site, stand as a reminder of the glory that prevailed during the Vijaynagara Empire. A visit to Hampi brings you across some of the best examples of aesthetics 
+                    and style in architecture. An example of that is the Virupaksha Temple, which consists of a Lord Shiva statue carved in stone. The other attractions 
+                    are the Queen’s Bath, Elephant Stables, Vijay Vittala Temple and Hampi Bazaar.<br>
+
+                    <b>Interesting Fact :</b>The first instance of human settlement dates back to 1 CE. 
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <h2 align="left">3.<u>Ellora Caves,Maharastra</u></h2>
+            <div class="col-12 box">
+                <img class="img" src="https://indianartzone.com/pub/media/catalog/product/e/l/ellora_caves_acrylic_on_canvas_36x48_210000.jpg" alt="Ellora">
+                <p align="left" id="han">
+                    <b><i>Ellora Caves</i></b> are a multi-religious rock-cut cave complex with inscriptions dating from the period 6th century CE onwards, located in the
+                     Aurangabad District of Maharashtra, India.They are also called verul caves.
+                     There are over 100 caves at the site, all excavated from the basalt cliffs in the Charanandri Hills, 34 of which are open to public.
+                     These consist of 17 Hindu (caves 13–29), 12 Buddhist (caves 1–12) and 5 Jain (caves 30–34) caves.Each group represents deities and mythologies prevalent 
+                     in the 1st millennium CE, as well as monasteries of each respective religion.They were built close to one another and illustrate the religious
+                     harmony that existed in ancient India.Because of their 
+                     exceptional architecture of ancient India, the Ellora Caves were inscribed on the UNESCO World Heritage List in 1983.<br>
+                     <b>Interesting fact:</b> Ellora is one of the largest rock-cut Hindu temple cave complexes in the world, and it is marvelous to say the least.
+                     
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <h2 align="left">4.<u>Hawa Mahal,Jaipur</u></h2>
+            <div class="col-12 box">
+                <img class="img" src="https://imgcld.yatra.com/ytimages/image/upload/t_yt_blog_c_fill_q_auto:good_f_auto_w_800_h_500/v1495524602/Hawa_Mahal_is_a_beautiful_palace_in_Jaipur.jpg" alt="Hawa">
+                <p align="left" id="han">
+                    Your tour is incomplete unless you visit the <b><i>Hawa Mahal</i></b> in Jaipur. Hawa Mahal, or the Palace of Winds, 
+                    was more of a gallery than a monument. It was designed by Lal Chand Ustad as a gallery from where ladies of the royal household could watch the everyday
+                    life in the street, since at other times, they had to observe the purdah. Built in red and pink sandstone, it’s shaped like a crown as the ruler of Jaipur during that time,
+                    Maharaja Sawai Pratap Singh, was a big devotee of Lord Krishna.<br><br>
+
+                    <b>Interesting Fact:</b>Hawa Mahal is the tallest building in the world without a proper foundation. It consists of 5 stories, but, without a proper foundation, leans at an angle of 87 degrees.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <h2 align="left">5.<u>Golconda,Telangana</u></h2>
+            <div class="col-12 box">
+                <img class="img" src="https://www.hyderabadtourism.travel/images/v2/header-places/golconda-fort-hyderabad-tourism-attraction-entryfee-timings-reviews-header.jpg" alt="Gol">
+                <p align="left" id="han">
+                    Derived from two words ‘Gola’ and ‘Konda’,<b><i> Golconda Fort</i></b> means ‘Shephard’s Hill’. 
+                    If you are an architecture buff, Golconda Fort is one of the fascinating historical places in India to visit. 
+                    Besides bewitched by every onlooker, this impressive structure stands majestically even today and is one of the Hyderabad’s 
+                    popular tourist places. Founded somewhere in the 12th century by the Kakatiya Kings, this fort was later expanded by the Qutub Shahi Kings.
+                    The best part of Golconda Fort is the water supply system that will leave you wondering at the level of advancement of the technical and scientific knowledge back in the day.<br><br>
+ 
+                    <b>Interesting Fact about Golconda Fort:</b> Hope Diamond, Darya-e Nur, Nur-Ul-Ain Diamond, Kohinoor, and Regent Diamond were owned by the sultans of Golconda before they moved out of India.
+                </p>    
+            </div>
+        </div>   
+        <div class="row">
+            <h2 align="left">6.<u>Churches and Convents,Goa </u></h2>
+            <div class="col-12 box">
+                <img class="img" src="https://static.toiimg.com/thumb/92691407/Basilica-of-Bom-Jesus.jpg?width=1200&height=900" alt="church">
+                <p align="left" id="han">
+                    <b><i>The churches and convents of Goa</i></b> are a big part of Goa tourism. In particular, the Church of Bom Jesus, which houses the tomb of 
+                    St. Francis Xavier and Se’ Cathedral. ‘Bom Jesus’ meaning ‘Infant Jesus’ or ‘Good Jesus’, Basilica of Bom Jesus is one of the most famous historical places in Goa 
+                    and is India’s first Minor Basilica. It is renowned in the Catholic world and is one of the finest examples of baroque architecture in the country. Apart from this, the Church and Convent 
+                    of St. Francis of Assisi, Church of Lady of Rosary; Church of St. Augustine are among some other well-known Churches and Convents of Goa.<br><br>
+
+                    <b>Interesting Fact:</b> The churches are convents of Goa were the former capital of Portuguese Indies and were persuasive in spreading forms of Manueline, Mannerist and Baroque art.
+                    
+                </p>    
+            </div>
+        </div>  
+        <div class="row">
+            <h2 align="left">7.<u>Cellular Jail, Andaman and Nicobar Islands</u></h2>
+            <div class="col-12 box">
+                <img class="img" src="https://ihplb.b-cdn.net/wp-content/uploads/2018/06/cellular-jail.jpg" alt="jail">
+                <p align="left" id="han">
+                    Went through a lot of bloodshed, <b><i>Cellular Jail</i></b> in the Andaman Islands was seen as the place to send anyone who rebelled against the British. The walls of this jail 
+                    has many untold stories of the brave Indian freedom fighters and prisoners who have encountered torment in the cells. Cellular Jail is also called Kala Pani because the 
+                    jail was surrounded by water on all sides so that prisoners couldn’t escape. It was constructed in the years 1896 and 1906 but the Britishers got hold of it during the late 1950s. And
+                    for years now, Cellular Jail has become one of the significant historical places to visit in India.<br><br>
+
+                    <b>Interesting Fact about Cellular Jail:</b> The jail has a total of 696 small-sized cells, which is 2.7 metres to 4.5 metres in size. The prisoners were kept in a way that no prisoner
+                     could see or talk to other prisoners.
+                    
+                </p>    
+            </div>
+        </div>  
+        <div class="row">
+            <h2 align="left">8.<u> Nalanda University, Bihar</u></h2>
+            <div class="col-12 box">
+                <img class="img" src="https://ihplb.b-cdn.net/wp-content/uploads/2018/06/nalanda-university.jpg" alt="uni">
+                <p align="left" id="han">
+                     <b><i>Nalanda University</i></b>,scholastic institution’s origins date back to the 5th century and it served as a highly recognized place of learning for an uninterrupted period of 800 years.
+                     After it was invaded three times, it fell into ruins and today, only a small portion of this huge campus has been excavated. A visit to the complex can prove to be an 
+                     enriching journey into history, architecture, culture, and Buddhism. If you are a history buff, this is one of India’s best historic places located in Bihar. Moreover, the ruins of 
+                     Nalanda was declared as a UNESCO World Heritage Site in India in 2016.<br><br>
+
+                     <b>Interesting Fact:</b> It was attacked three times by the invaders – the Gaudas, the Huns, and Bhaktiyar Khilji, which brought its annihilation.
+                </p>    
+            </div>
+        </div> <br><br>
+        <p align="center" id="han">
+        <i><b>
+            Thus, set out and explore the various historical places in India that are enriched with culture. Uncover the diversity of this magnificent country and some exquisite architectural beauty. 
+            From intricately decorated windows to massive sculptural inscriptions, all these monuments display artistic prowess at its best.Get lost in the ancient world of historic monuments as you explore
+             the dynamic Indian heritage on your holiday in India.</b>
+         </i><br><br>
+         ************    
+        </p> <br>
+    
+    <footer>
+        <h3> Designed and Developed by SUBHASHINI</h3>
+    </footer>
+    </div>
+    
+    </div>       
+    </div>
+    </body>
+</html>
